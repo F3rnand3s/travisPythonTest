b039dfb6bb1a1ac547eece740cb75f79d3d8db3c
F3rnand3s <f3rnand3s@hotmail.com>
2020-03-26 13:20:13 +0000

    small update on unittesting

--- unitTesting.py
+++ unitTesting.py
@@ -2,16 +2,13 @@ import unittest
 
 class unitTesting(unittest.TestCase): 
     def setUp(self): 
-        print 'some text 1'
+        print 'some test 1'
 
     def test_search_in_python_org(self):
-        print 'some text 2'
+        print 'some test 2'
 
     def tearDown(self):
-        print 'some text 3'
-        print 'some text 4'
-        print 'some text 5'
+        print 'some test 3'
 
 if __name__ == "__main__":
     unittest.main()
-
